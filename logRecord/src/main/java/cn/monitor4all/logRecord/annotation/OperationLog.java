81228ed0155fee6c398a5a27117ef30ffb8b044b
zhendongyang <yangzd1993@foxmail.com>
2021-09-20 18:01:45 +0800

    feat: add structure

---
+++ logRecord/src/main/java/cn/monitor4all/logRecord/annotation/OperationLog.java
@@ -0,0 +1,4 @@
+package cn.monitor4all.logRecord.annotation;
+
+public @interface OperationLog {
+}
---
+++ logRecord/src/main/java/cn/monitor4all/logRecord/service/impl/QueueLogServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.monitor4all.logRecord.service.impl;
+
+import cn.monitor4all.logRecord.bean.LogDTO;
+import cn.monitor4all.logRecord.service.LogService;
+
+public class QueueLogServiceImpl implements LogService {
+    public boolean createLog(LogDTO logDTO) {
+        return false;
+    }
+}
