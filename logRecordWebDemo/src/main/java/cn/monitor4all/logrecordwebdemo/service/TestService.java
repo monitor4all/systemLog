b795bf4f4b66f401aa5ddb4f86632881a01c13aa
zhendongyang <yangzd1993@foxmail.com>
2021-11-18 20:46:38 +0800

    fix: logRecord 删除Pipeline属性

--- logRecordWebDemo/src/main/java/cn/monitor4all/logrecordwebdemo/service/TestService.java
+++ logRecordWebDemo/src/main/java/cn/monitor4all/logrecordwebdemo/service/TestService.java
@@ -1,6 +1,5 @@
 package cn.monitor4all.logrecordwebdemo.service;
 
-import cn.monitor4all.logRecord.annotation.DataPipelineEnum;
 import cn.monitor4all.logRecord.annotation.OperationLog;
 import cn.monitor4all.logrecordwebdemo.domain.TestClass;
 import org.springframework.stereotype.Service;
@@ -8,9 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
 
-    @OperationLog(bizId = "#testClass.testStr", bizType = "testType1", msg = "#testClass", pipeline = DataPipelineEnum.QUEUE)
-    @OperationLog(bizId = "#testClass.testStr", bizType = "testType2", msg = "#testClass", pipeline = DataPipelineEnum.QUEUE)
-    @OperationLog(bizId = "#testClass.testStr", bizType = "testType3", msg = "#testClass", pipeline = DataPipelineEnum.QUEUE)
+    @OperationLog(bizId = "#testClass.testStr", bizType = "testType1", msg = "#testClass")
+    @OperationLog(bizId = "#testClass.testStr", bizType = "testType2", msg = "#testClass")
+    @OperationLog(bizId = "#testClass.testStr", bizType = "testType3", msg = "#testClass")
     public String testService(TestClass testClass, boolean isSuccess) throws Exception {
         if (isSuccess) {
             return testClass.toString();
